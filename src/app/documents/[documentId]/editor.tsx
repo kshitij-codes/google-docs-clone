c25bbd73056d2e5851bfe0314a995bb03c85dc9b
Kshitij <kshitijkvy@gmail.com>
2025-06-15 00:51:07 +0530

    feat: editor and basic toolbar functionality setup

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import {useEditor, EditorContent} from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import TaskList from '@tiptap/extension-task-list';
+import TaskItem from '@tiptap/extension-task-item';
+import Image from '@tiptap/extension-image';
+import Table from '@tiptap/extension-table';
+import TableRow from '@tiptap/extension-table-row';
+import TableHeader from '@tiptap/extension-table-header';
+import TableCell from '@tiptap/extension-table-cell';
+import ImageResize from 'tiptap-extension-resize-image'
+import { useEditorStore } from '@/store/use-editor-store';
+import Underline from '@tiptap/extension-underline';
+
+
+const Editor = () => {
+
+  const { setEditor } = useEditorStore();
+
+  const editor = useEditor({
+    onCreate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onSelectionUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onTransaction: ({ editor }) => {
+      setEditor(editor);
+    },
+    onFocus: ({ editor }) => {
+      setEditor(editor);
+    },
+    onBlur: ({ editor }) => {
+      setEditor(editor);
+    },
+    onContentError: ({ editor }) => {
+      setEditor(editor);
+    },
+    editorProps: {
+      attributes: { 
+        style: 'padding-left: 56px; padding-right: 56px;',
+        class: 'focus:outline-none print:border-0 border bg-white border=[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-14 pb-10 cursor-text',
+      },
+    },
+    extensions: [
+        StarterKit,
+        Underline,
+        Image,
+        ImageResize,
+        Table,
+        TableRow,
+        TableHeader,
+        TableCell,
+        TaskList,
+        TaskItem.configure({
+            nested: true
+        })
+    ],
+    content: '<p>Hello World! 🌍</p>'
+});
+
+  return (
+    <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:bg-white print:p-0 print:overflow-visible'>
+      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  )
+}
+
+export default Editor
---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Editor from './editor'
+import Toolbar from './toolbar'
+
+const DocumentIdPage = () => {
+  return (
+    <div>
+    <Toolbar />
+      <Editor />
+    </div>
+  )
+}
+
+export default DocumentIdPage
