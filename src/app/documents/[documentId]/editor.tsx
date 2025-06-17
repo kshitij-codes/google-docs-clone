9807be92e59034d9e5d61037292c09079df49e9f
Kshitij <kshitijkvy@gmail.com>
2025-06-17 13:01:28 +0530

    feat: margin ruler, dynamic navigation bar component and its tools

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,12 +20,14 @@ import Link from '@tiptap/extension-link';
 import TextAlign from "@tiptap/extension-text-align";
 import { FontSizeExtension } from '@/extensions/font-size';
 import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 const Editor = () => {
 
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender: false,
     onCreate: ({ editor }) => {
       setEditor(editor);
     },
@@ -60,7 +62,7 @@ const Editor = () => {
         StarterKit,
         LineHeightExtension.configure({
           types: ["heading", "paragraph"],
-          defaultLineHeight: "Normal"
+          defaultLineHeight: "Normal",
         }),
         FontSizeExtension,
         TextAlign.configure({
@@ -95,6 +97,7 @@ const Editor = () => {
 
   return (
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:bg-white print:p-0 print:overflow-visible'>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Editor from './editor'
 import Toolbar from './toolbar'
+import { Navbar } from './navbar'
 
 const DocumentIdPage = () => {
   return (
     <div>
-    <Toolbar />
-      <Editor />
+      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden h-[112px]">
+        <Navbar />
+        <Toolbar />
+      </div>
+      <div className='pt-[114px] print:pt-0'>
+        <Editor />
+      </div>
     </div>
   )
 }
