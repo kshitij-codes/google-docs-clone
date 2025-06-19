e24802ec4620178aa736cfb7d83b093e1b34cccc
Kshitij <kshitijkvy@gmail.com>
2025-06-19 22:47:46 +0530

    feat: Implemented remove and rename functionality, and lazy loading

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,15 +3,12 @@
 import { Navbar } from './navbar';
 import { TemplatesGallery } from './templates-gallery';
 import { api } from '../../../convex/_generated/api';
-import { useQuery } from 'convex/react';
+import { usePaginatedQuery, useQuery } from 'convex/react';
+import { DocumentsTable } from './documents-table';
 
 const Home = () => {
-  const documents = useQuery(api.documents.get)
-  if(documents === undefined) {
-    return (
-      <p>Loading...</p>
-    )
-  }
+  const {results, status, loadMore} = usePaginatedQuery(api.documents.get, {}, {initialNumItems: 5})
+
     return (
         <div className="min-h-screen flex flex-col">
           <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
@@ -19,9 +16,11 @@ const Home = () => {
           </div>
           <div className="mt-16">
             <TemplatesGallery />
-            {documents?.map((document) => (
-              <span key={document._id}>{document.title}</span>
-            ))}
+            <DocumentsTable
+              documents={results}
+              loadMore={loadMore}
+              status={status}
+            />
           </div>
         </div>
     )
--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -3,11 +3,27 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import { templates } from "@/constants/templates"
 import { cn } from "@/lib/utils"
+import { useMutation } from "convex/react"
+import { useRouter } from "next/navigation"
+import { api } from "../../../convex/_generated/api"
+import { useState } from "react"
 
 
 export const TemplatesGallery = () => {
+    const router = useRouter()
+    const create = useMutation(api.documents.create)
+    const [isCreating, setIsCreating] = useState(false)
 
-    const isCreating = false
+    const onTemplateClick = (title: string, initialContent: string) => {
+        setIsCreating(true)
+        create({title, initialContent})
+            .then((documentId) => {
+                router.push(`/documents/${documentId}`)
+            })
+            .finally(() => {
+                setIsCreating(false)
+            })
+    }
 
     return (
         <div className="bg-[#F1F3F4]">
@@ -26,7 +42,8 @@ export const TemplatesGallery = () => {
                                 )}>
                                     <button 
                                         disabled={isCreating} 
-                                        onClick={()=>{}} 
+                                        // TODO: Add proper initial content
+                                        onClick={()=> onTemplateClick(template.label, "")}
                                         style={{
                                             backgroundImage: `url(${template.imageUrl})`,
                                             backgroundSize: "cover",
